20ed057d97563f2a7d95a1c0ed8e5651769631fb
LEEDONHYUEN <88918912+LEEDONHYUEN@users.noreply.github.com>
2021-08-28 13:36:07 +0900

    Update first_file.py

--- first_file.py
+++ first_file.py
@@ -102,3 +102,61 @@ for j in range(100):
     c = random.randrange(1,100)
     b.append(c)
 print(a,bssaaeeww)
+
+
+
+
+#     print("prime")
+# else:
+#     print("not prime")
+#
+# for i in range(13):
+#     if i==10:
+#         continue
+#         print(i)
+
+# b=[]
+# c=[1,2,3,4,5]
+#
+# b.append(1)
+# c.append(2)
+# import random
+#
+# a = random.randrange(1,100)
+# print(a)
+
+import random
+a=[]
+b = []
+a.append(1*10)
+
+for i in range(100):
+    a.append(i)
+
+for j in range(100):
+    c = random.randrange(1,100)
+    b.append(c)
+print(a,b)
+
+i=0
+#while i < 5:
+    #print("hi",i)
+    #i=i+1
+while i<100:
+
+    i=i+1
+    if i%3==0:
+        print("*")
+    elif i%3!=0:
+        print(i)
+i=0
+while i <11:
+    print("*"*(11-i))
+    i = i + 1
+
+for i in range(1,100):
+
+        one=i%10
+        ten=i//10
+        first_if=one!0 and ten !=0=and one% 3==0
+        second_if=ten %3==
